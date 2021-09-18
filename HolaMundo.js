7855491d7ac91a63179954e911c6a39cde6e3154
Ruben <conrderuben@gmail.com>
2021-09-18 12:41:55 +0200

    casa 18/09

--- HolaMundo.js
+++ HolaMundo.js
@@ -2,4 +2,5 @@ alert("Hola mundo");
 var NOMBRE="Juan";
 var edad=16;
 var resultado="El usuario " + NOMBRE + " tiene una edad de " + edad + " años"
-alert(resultado);
+alert(resultado); 
+//Pruebas
