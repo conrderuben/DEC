046fd8a4e483472a413e0ef6f3189f9c3be2c7b4
Ruben <conrderuben@gmail.com>
2021-09-18 12:50:46 +0200

    18/09 CASA

--- EjemploVariables.js
+++ EjemploVariables.js
@@ -2,4 +2,5 @@
 	// typeof variable; o typeof(variable); -- Devuelve el tipo de dato 
 	// document.write("<h1>Hola</h1>"); 
 
+	
     
--- HolaMundo.js
+++ HolaMundo.js
@@ -3,4 +3,3 @@ var NOMBRE="Juan";
 var edad=16;
 var resultado="El usuario " + NOMBRE + " tiene una edad de " + edad + " años"
 alert(resultado); 
-//Pruebas
